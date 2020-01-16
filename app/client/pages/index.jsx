bf9ed49f0f70289eec53d0080d9b6f459c40cf32
Smit Ray <humancoder@outlook.com>
2020-01-17 03:11:16 +0530

    style(mui updated): mui updated
    Material UI installed and configured

--- app/client/pages/index.jsx
+++ app/client/pages/index.jsx
@@ -1,8 +1,32 @@
-import Header from 'components/Header';
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import MuiLink from '@material-ui/core/Link';
 
-export default () => (
-  <>
-    <Header />
-    <h1>Hi this is a test</h1>
-  </>
-);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <MuiLink color="inherit" href="https://material-ui.com/">
+        Your Website
+      </MuiLink>
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
